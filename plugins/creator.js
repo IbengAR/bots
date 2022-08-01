cf2e8cbe2cfbd656e52634dae8b375cf6e0b19b8
IbengAR <56824876+IbengAR@users.noreply.github.com>
2022-08-01 20:55:49 +0800

    Add files via upload

---
+++ plugins/creator.js
@@ -0,0 +1,15 @@
+const PhoneNumber = require('awesome-phonenumber')
+async function handler(m) {
+                let vcard = 'BEGIN:VCARD\n' // metadata of the contact card
+                    + 'VERSION:3.0\n'
+                    + 'Rizky iBeng\n' // full name
+                    + 'TEL;type=CELL;type=VOICE;waid=6281257172080:+62 895-0339-8304\n' // WhatsApp ID + phone number
+                    + 'END:VCARD'
+                conn.sendMessage(m.chat, { contacts: { displayName: 'Rizky iBeng', contacts: [{ vcard }] } }, { quoted: m })
+}
+handler.help = ['owner', 'creator']
+handler.tags = ['info']
+
+handler.command = /^(owner|creator)$/i
+
+module.exports = handler
