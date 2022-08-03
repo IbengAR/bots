62ca3edb09b3e3abe52869230ef57b4a2cf1c2cd
ibengar <56824876+ibengar@users.noreply.github.com>
2022-08-03 08:37:58 +0800

    Update mkick.js

--- plugins/mkick.js
+++ plugins/mkick.js
@@ -3,7 +3,7 @@ let who = m.mentionedJid[0] ? m.mentionedJid[0] : m.qouted ? m.quoted.sender : '
 await conn.groupRemove(m.chat, [who || m.quoted.sender])
 }
 handler.help = ['mkick']
-handler.tags = ['mods']
+handler.tags = ['owner']
 handler.command = /^(mkick)$/i
 
 handler.group = true
